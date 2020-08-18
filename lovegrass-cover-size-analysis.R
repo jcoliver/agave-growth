dffd14661ddf123a063303ab9e6786bf108f1136
Jeff Oliver <jcoliver@email.arizona.edu>
2020-08-17 17:23:13 -0700

    Add Lovegrass cover size plot

--- lovegrass-cover-size-analysis.R
+++ lovegrass-cover-size-analysis.R
@@ -6,4 +6,90 @@
 rm(list = ls())
 
 ################################################################################
-# Corresponds to 2.1.b in report, but may need to adjust based on assumptions
+# Use linear regression to predict percent cover of Lehmann lovegrass from 
+# treatment, number of agave leaves, and interaction. Only considering control 
+# and hand-pulling treatments
+
+# Roughly corresponds to 2.1.b in report but results here accommodate 
+# heteroscedasticity
+
+library(car)         # Levene test of homogeneity of variance
+library(MASS)        # studentized residuals
+library(lmtest)      # accommodate heteroscedasticity in standard error estimate
+library(sandwich)    # White's estimator for standard errors
+library(dplyr)       # data wrangling
+library(broom)       # tidy up output from statistical output
+library(stringr)     # clean up statistical output
+
+# Cover ~ Treatment + Leaves + Treatment x Leaves
+# Continuous ~ Categorical + Continuous
+
+cover_data <- read.csv(file = "data/agave-size-data.csv", 
+                       stringsAsFactors = FALSE)
+
+# Restrict data to only control and hand-pulling
+cover_data <- cover_data %>%
+  filter(Treatment %in% c("C", "H"))
+
+cover_model <- lm(formula = aerial_cover ~ Treatment * live_leaf_number,
+                  data = cover_data)
+
+# Check assumptions of linear regression
+# Look at fitted vs. residuals
+plot(cover_model, which = 1)
+# Flagged 5, 6, 13 as having "large" residuals
+
+# Q-Q plot for normality of residuals
+plot(cover_model, which = 2)
+# Flagged 6, 12, 13 as deviations from expectations. Plot the distribution 
+# of residuals
+cover_sresids <- MASS::studres(cover_model)
+hist(cover_sresids, freq = FALSE)
+xfit <- seq(min(cover_sresids), max(cover_sresids), length = 50)
+yfit <- dnorm(xfit)
+lines(xfit, yfit)
+# Meh. Small sample size, but not terrible.
+
+# Heteroscedasticity
+plot(cover_model, which = 3)
+# 6, 12, 13 a bit high, and line may have positive slope. Do test
+car::ncvTest(model = cover_model)
+# Non-constant Variance Score Test 
+# Variance formula: ~ fitted.values 
+# Chisquare = 7.946772, Df = 1, p = 0.0048173
+# Looks heteroscedastic. Quite.
+
+# Influential outliers
+plot(cover_model, which = 5)
+# Some observations close to 0.5 Cook's distance. Do an outlier test
+outlierTest(model = cover_model)
+# No Studentized residuals with Bonferroni p < 0.05
+# Largest |rstudent|:
+#   rstudent unadjusted p-value Bonferroni p
+# 6 3.367927          0.0055916     0.095057
+# Looks fine.
+
+# Homoscedasticity violated, so need better standard error estimates
+# use a heteroscedasticity constant model of variance with White's estimator
+cover_vcovHC <- lmtest::coeftest(x = cover_model,
+                                 vcov = sandwich::vcovHC(x = cover_model,
+                                                         type = "HC0"))
+
+# Tidy the output so we can write to file
+cover_out <- broom::tidy(cover_vcovHC)
+
+# Remove the word "Treatment"
+cover_out <- cover_out %>%
+  mutate(term = stringr::str_replace(string = term,
+                                     pattern = "Treatment",
+                                     replacement = ""))
+
+# Change "statistic" column to z.value and "term" to coefficient
+cover_out <- cover_out %>%
+  rename(z.value = statistic,
+         coefficient = term)
+
+# Write output to file
+write.csv(x = cover_out, 
+          file = "output/lovegrass-cover-size-analysis-out.csv",
+          row.names = FALSE)
--- lovegrass-cover-size-plot.R
+++ lovegrass-cover-size-plot.R
@@ -6,3 +6,63 @@
 rm(list = ls())
 
 ################################################################################
+# Corresponds to Figure 2.1.b in report
+
+library(dplyr)       # data wrangling
+library(ggplot2)     # plotting
+library(extrafont)   # Times New Roman font
+
+cover_data <- read.csv(file = "data/agave-size-data.csv", 
+                       stringsAsFactors = FALSE)
+
+# Restrict data to only control and hand-pulling
+cover_data <- cover_data %>%
+  filter(Treatment %in% c("C", "H"))
+
+# Run regression model for parameter estimates
+cover_model <- lm(formula = aerial_cover ~ Treatment * live_leaf_number,
+                  data = cover_data)
+
+# Plot the model results
+# Predict lines of relationships
+control_limits <- c(min(cover_data$live_leaf_number[cover_data$Treatment == "C"]),
+                    max(cover_data$live_leaf_number[cover_data$Treatment == "C"]))
+control_line <- data.frame(Treatment = "C",
+                           live_leaf_number = control_limits)
+control_line$aerial_cover <- predict(cover_model, 
+                                     newdata = control_line)
+
+hand_pulling_limits <- c(min(cover_data$live_leaf_number[cover_data$Treatment == "H"]),
+                         max(cover_data$live_leaf_number[cover_data$Treatment == "H"]))
+hand_pulling_line <- data.frame(Treatment = "H",
+                                live_leaf_number = hand_pulling_limits)
+hand_pulling_line$aerial_cover <- predict(cover_model, 
+                                          newdata = hand_pulling_line)
+
+# Plot the data
+cover_plot <- ggplot(data = cover_data, 
+                                 mapping = aes(x = live_leaf_number, 
+                                               y = aerial_cover,
+                                               group = Treatment, 
+                                               fill = Treatment,
+                                               shape = Treatment)) +
+  geom_line(data = control_line, color = "#787878") +
+  geom_line(data = hand_pulling_line, color = "#D8B365") +
+  geom_point(size = 2.0, alpha = 0.75) +
+  scale_fill_manual(values = c("#787878", "#D8B365"),
+                    labels = c("Control", "Hand-pulling")) +
+  scale_shape_manual(values = c(21, 22), # Circle, square
+                     labels = c("Control", "Hand-pulling")) +
+  xlab(label = "Number of leaves") +
+  ylab(label = "Percent cover") +
+  ggtitle(label = "Figure 2.1.b. Percent Lehmann cover by agave size") +
+  theme_bw() +
+  theme(text = element_text(family = "Times New Roman"))
+# print(cover_plot)
+
+# Print to file
+ggsave(filename = "output/lovegrass-cover-size-plot.pdf", 
+       plot = cover_plot,
+       width = 6.5,
+       height = 3,
+       units = "in")
